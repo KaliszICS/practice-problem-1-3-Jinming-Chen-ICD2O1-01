dda852eff32b6c01b498099129016eb589f7fabe
Jinming-Chen-ICD2O1-01 <jinming.chen@student.tdsb.on.ca>
2024-11-05 09:35:35 -0500

    Update PP1_3.py

--- PP1_3.py
+++ PP1_3.py
@@ -1,14 +1,57 @@
-
+''' 
+    Lesson 3 - Using Strings
+    Author: Jinming Chen
+    Date Created: Nove 05th, 2024 
+    Date Last Modified: Nove o5th, 2024 
+'''
 
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
+  # Output shape a
+print(" * * * * * *")
+print("* * * * * * ")
+print(" * * * * * * ")
+print(" * * * * * * ")
+print(" * * * * * * ")
+print("* * * * * * ")
+
+# Output a blank line to separate it from the next shape
+print()
+
+# Output shape b
+print("* * * * *")
+print("*       *")
+print("*       *")
+print("*       *")
+print("* * * * *")
+
+# Output a blank line to separate it from the next shape
+print()
+
+# Output shape c
+print("* * * * * *")
+print(" *       * ")
+print("  *     *  ")
+print("   *   *   ")
+print("    * *    ")
+print("     *     ")
+# Output a blank line to separate from the next output
+print()
 
 def q2():
   #Write Assignment code here
-  
+
+  # Output "Hello World" using two print statements, ensuring that the output is on the same line
+print("Hello ", end="")
+print("World")
+# Output a blank line to separate from the next output
+print()
+
 def q3():
   #Write Assignment code here
+  # Output the following using one print statement, with each word on a different line and including quotes
+print('"Man"\n"Board"\n"Man Overboard"')
   
 
 
